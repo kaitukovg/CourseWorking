9a0c7c861e6ad8a5e49bfa14454347470764acb0
kaitukovg <germonskiy@gmail.com>
2025-12-07 14:20:55 +0300

    Create Button.h

---
+++ XSmall-HMI-Player/include/Button.h
@@ -0,0 +1,46 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "VisualObject.h"
+#include "VariableDatabase.h"
+#include <SFML/Graphics.hpp>
+#include <functional>
+#include <string>
+
+class Button : public VisualObject {
+private:
+    sf::RectangleShape shape;
+    sf::Text text;
+    sf::Font* font;
+    std::string variableName;
+    sf::Color normalColor;
+    sf::Color hoverColor;
+    sf::Color pressedColor;
+    sf::Color textColor; 
+    bool isHovered;
+    bool isPressed;
+    std::function<void()> onClick;
+    
+    // Новое поле для идентификации действия
+    std::string actionType;
+
+public:
+    Button(float x, float y, float width, float height,
+           const std::string& buttonText, sf::Font* font, unsigned int fontSize,
+           const sf::Color& color, const std::string& name, VariableDatabase* db,
+           const std::string& varName = "", std::function<void()> onClickFunc = nullptr,
+           const sf::Color& textClr = sf::Color::Black);  
+    
+    void draw(sf::RenderWindow& window) override;
+    void update() override;
+    void handleEvent(const sf::Event& event, sf::RenderWindow& window) override;
+    
+    bool contains(float pointX, float pointY) const;
+    void setOnClick(std::function<void()> onClickFunc);
+    void setTextColor(const sf::Color& color);
+    
+    // Новый метод для установки действия
+    void setAction(const std::string& action, VariableDatabase* db);
+};
+
+#endif
